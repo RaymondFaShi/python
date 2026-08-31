70d638009b7c90d541f07346595562ca019ca0e6
raymond <raymond@192.168.28.100>
2026-08-31 09:39:52 +0800

    add

--- app/main.py
+++ app/main.py
@@ -5,13 +5,10 @@ from PySide6.QtWidgets import QApplication, QMessageBox;
 from app.bootstrap import Bootstrap;
 
 def main():
-    try:
-        app = QApplication();
-        bootstrap = Bootstrap();
-        bootstrap.run();
-        app.exec();
-    except:
-        print( '未知错误' );
+    app = QApplication();
+    bootstrap = Bootstrap();
+    bootstrap.run();
+    app.exec();
 
 
 
